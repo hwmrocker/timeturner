c62619fb1fbaadc479237645399cf72fc549251e
Olaf Gladis <hwmrocker@gladis.org>
2025-06-19 11:45:54 +0200

    Switch dev dependencies from [dependency-groups] to [tool.uv]
    Update test_version.py to use project.version
    
    Regenerate uv.lock with updated dev dependencies

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,4 +5,4 @@ from timeturner import __VERSION__
 
 def test_version():
     pyproject = tomlkit.parse(open("pyproject.toml").read())
-    assert __VERSION__ == pyproject["tool"]["poetry"]["version"]  # type: ignore
+    assert __VERSION__ == pyproject["project"]["version"]  # type: ignore
